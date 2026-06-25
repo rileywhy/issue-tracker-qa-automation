45a2fa70d51972e32ac14d83ec2fdd5257da8ea2
cdyoun2 <80136119+cdyoun2@users.noreply.github.com>
2026-06-24 20:33:37 -0600

    First working playwright test. super basic

--- frontend/tests/auth.spec.ts
+++ frontend/tests/auth.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from "@playwright/test";
 test("login", async ({ page }) => {
   await page.goto("http://localhost:5173/login");
 
-  await page.getByLabel("email").fill("johnsmith@test.com");
+  await page.getByPlaceholder("Email").fill("johnsmith@test.com");
 
-  await page.getByLabel("password").fill("password");
+  await page.getByPlaceholder("Password").fill("password");
 
-  await page.getByRole("button", {name: "submit"}).click();
+  await page.getByRole("button", ).click();
 
   await expect(page.getByText("john smith")).toBeVisible(); 
 
