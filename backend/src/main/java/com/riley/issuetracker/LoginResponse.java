8005785ff7f8de54c321f569ccdc376d6d453ea0
rileywhy <rileyyoung512@gmail.com>
2026-06-26 18:20:29 -0600

    JWT authentication added

--- backend/src/main/java/com/riley/issuetracker/LoginResponse.java
+++ backend/src/main/java/com/riley/issuetracker/LoginResponse.java
@@ -4,11 +4,13 @@ public class LoginResponse {
     private String firstName;
     private String lastName;
     private String email;
+    private String token;
 
-    public LoginResponse(String firstName, String lastName, String email) {
+    public LoginResponse(String firstName, String lastName, String email, String token) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.token = token;
     }
 
     public String getFirstName() {
@@ -21,5 +23,10 @@ public class LoginResponse {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getToken()
+    {
+        return token;
     }
 }
